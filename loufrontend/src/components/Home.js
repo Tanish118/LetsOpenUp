5ac9f9fe86126717301183f022e74be4f93f3d30
Tanish Sharma <70182737+Tanish118@users.noreply.github.com>
2021-05-11 16:52:30 +0530

    T1- homepage
    setting body container and other attributes

--- loufrontend/src/components/Home.js
+++ loufrontend/src/components/Home.js
@@ -3,10 +3,49 @@ import styled from'styled-components'
 
 function Home(props) {
     return (
-        <div>
-            Home
-        </div>
+     <Container>
+         <Section>
+             <h5>
+                 <a>Hiring in a hurry?-</a></h5>
+                 <p>Find Talented in record and keep it moving</p>
+             
+         </Section>
+
+     </Container>
     )
 }
 
 export default Home
+const Container=styled.div`
+    padding:52px;
+    max-width:100%;
+
+`;
+const Content=styled.div`
+max-width:1128px;
+margin-left:auto;
+margin-right:auto;
+`;
+const Section=styled.div`
+min-height:50px;
+padding :16px 0;
+box-sizing:content-box;
+text-align:center;
+text-decoration:underline;
+display:flex;
+justify-content:center;
+h5{
+    color:blue;
+    font-weight:700px;
+}
+p{
+    font-size:16px;
+    color:#434649;
+    font-weight:600px;
+}
+@media(max-width:768px){
+    flex-direction:column;
+    padding:0 5px;
+}
+
+`;
