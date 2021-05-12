42d1f719a2df6ac6ebddb9872ac6e4a40d116b68
Tanish Sharma <70182737+Tanish118@users.noreply.github.com>
2021-05-12 10:43:08 +0530

    T1-postbox

--- loufrontend/src/components/Main.js
+++ loufrontend/src/components/Main.js
@@ -27,6 +27,42 @@ function Main(props) {
                    </button>
                </div>
             </Sharebox>
+            <div>
+                <Article>
+                    <SharedActor>
+                        <a>
+                            <img src="/images/user.svg" alt="" />
+                            <div>
+                                <span>Title</span>
+                                <span>Info</span>
+                                <span>Date</span>
+                            </div>
+                        </a>
+                        <button>
+                            <img src="/images/more.svg" alt="" height="30" width="30"/>
+                        </button>
+                    </SharedActor>
+                    <Description>
+                        Text
+                    </Description>
+                    <SharedImg>
+                        <a>
+                            <img src="/images/postbg.jpg" alt=""/>
+                        </a>
+                    </SharedImg>
+                    <SocialCount>
+                        <li><button>
+                            <img src="/images/flikes.svg" alt="" width="20" height="15"/>
+                         
+                            <img src="/images/react-clap.svg" alt="" width="20" height="15"/>
+                            <span>75</span>
+                            </button></li>
+                        <li><a>
+                           <span>2 comments</span>
+                            </a></li>
+                    </SocialCount>
+                </Article>
+            </div>
         </Container>
     )
 }
@@ -96,6 +132,101 @@ const Sharebox=styled(CommonCard)`
                     color:#70b5f9;
                 }
             }
+        }
+    }
+`;
+const Article=styled(CommonCard)`
+    padding:0;
+    margin:0 0 8px;
+    overflow:visible;
+
+`;
+const SharedActor=styled.div`
+    padding-right:40px;
+    flex-wrap:nowrap;
+    padding :12px 16px 0;
+    margin-bottom:8px;
+    align-items:center;
+    display:flex;
+    a{
+        margin-right:12px;
+        flex-grow:1;
+        overflow:hidden;
+        display:flex;
+        text-decoration:none;
+
+        img{
+            width:48px;
+            height:48px;
+        }
+        &>div{
+            display:flex;
+            flex-direction:column;
+            flex-grow:1;
+            flex-basis:0;
+            margin-left:8px;
+            overflow:hidden;
+            span{
+                text-align:left;
+                &:first-child{
+                    font-size:14px;
+                    font-weight:700;
+                    color:rgba(0,0,0,1);
+                }
+                &:nth-child(n+1){
+                    font-size:12px;
+                    color:rgba(0,0,0,0.6);
+                }
+            }
+        }
+    }
+    button{
+        position:relative;
+        right:12px;
+        top:0;
+        background:transparent;
+        border:none;
+        outline:none;
+    }
+
+`;
+const Description=styled.div`
+    padding:0 16px;
+    overflow:hidden;
+    color:black;
+    font-size:14px;
+    text-align:left;
+
+`;
+
+const SharedImg=styled.div`
+    margin-top:8px;
+    width:100%;
+    display:block;
+    position:relative;
+    background-color:#f9fafb;
+    img{
+        object-fit:contain;
+        width:100%;
+        height:100%;
+    }
+`;
+const SocialCount=styled.ul`
+    line-height:1.3;
+    display:flex;
+    align-items:flex-start;
+    overflow:auto;
+    margin:0 16px;
+    padding:8px 0;
+    border-bottom:1px solid #e9e5df;
+    list-style:none;
+    li{
+        align-items:center;
+        margin-right:5px;
+        font-size:12px;
+        button{
+           display:flex;
+           border:none;
         }
     }
 `;
