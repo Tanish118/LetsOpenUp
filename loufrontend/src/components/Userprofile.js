3523ace91af659b060df21b5d4f82de671125735
AKAkAraN <69759152+AKAkAraN@users.noreply.github.com>
2021-05-20 18:24:11 +0530

    user profile
    GG

--- loufrontend/src/components/Userprofile.js
+++ loufrontend/src/components/Userprofile.js
@@ -1,46 +1,53 @@
 import React from 'react'
-import styled from "styled-components";
 
 export default function profile(props){
     return (
-        <div style={{maxWidth:"550px",margin:"0px auto"}}>
+        <div style={{maxWidth:"700px",margin:"0px auto"}}>
             <div style={{
                 display:"flex",
                 justifyContent:"space-around",
                 margin:"70px 0px",
-                borderBottom:"1px solid grey"
+                borderBottom:"1px solid grey",
+                bottom: 0,
+                top:0,
+                right:0,
+                left:0,
+                content: '',
             }}>
                 <div>
-                    <img style={{width:"180px",height:"180px",borderRadius:"90px"}}
-                    src="https://images.unsplash.com/photo-1608127347890-3fd1d0323b3f?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTh8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='stick'/>
+                    <img style={{width:"160px",height:"160px",borderRadius:"90%"}}
+                    src="https://images.unsplash.com/photo-1617350697117-536a7e59a2cd?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM5MHx0b3dKWkZza3BHZ3x8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='stick'/>
                 </div>
                 <div>
-                    <h1>banda allah ka</h1>
-                    <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
-                        <h5>50 posts</h5>
-                        <h5>45 followers</h5>
-                        <h5>40 following</h5>
+                    <div className="picture">Peter mckinnon </div>
+                    <div style={{
+                    display:"flex",
+                    justifyContent:"space-around",
+                    width:"108%",
+                }} >
+                        <p>5 posts</p>
+                        <p>64 followers</p>
+                        <p>67 following</p>
                     </div>
 
                 </div>
             </div>
-            <Container>
+            <div className="gallery">
 
-                <img  src="https://images.unsplash.com/photo-1621324124020-fb7e2571a600?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
-                <img  src="https://images.unsplash.com/photo-1621324124020-fb7e2571a600?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
-                <img  src="https://images.unsplash.com/photo-1621324124020-fb7e2571a600?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
-                <img  src="https://images.unsplash.com/photo-1621324124020-fb7e2571a600?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
+                <img  className="item" src="https://images.unsplash.com/photo-1621354236838-78d7d91dd908?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
+                <img  className="item" src="https://images.unsplash.com/photo-1621354236838-78d7d91dd908?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
+                <img  className="item" src="https://images.unsplash.com/photo-1621354236838-78d7d91dd908?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
+                <img  className="item" src="https://images.unsplash.com/photo-1621354236838-78d7d91dd908?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
+                <img  className="item" src="https://images.unsplash.com/photo-1621354236838-78d7d91dd908?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
+                <img  className="item" src="https://images.unsplash.com/photo-1621354236838-78d7d91dd908?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
+                <img  className="item" src="https://images.unsplash.com/photo-1621354236838-78d7d91dd908?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='thick' />
               
-            </Container>
+            </div>
         </div>
 
     )
 
 }
-const Container = styled.div`
-display: flex;
-flex-wrap: wrap;
-/* justify-content: ; */
-`
+
 
     
